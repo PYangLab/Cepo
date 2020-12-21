3a3776d227a5126d62c5d7351e6d39dda1fa2ae9
hani <hani.kim127@gmail.com>
2020-12-21 17:53:11 +1100

    add dummy data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' @title cellbench
+#'
+#' @description A single-cell RNA-seq dataset adapted from [sc_mixology](https://github.com/LuyiTian/sc_mixology)
+#'
+#' @usage data(Cellbench)
+#' 
+#' @source https://github.com/LuyiTian/sc_mixology
+#'
+#' @format An object of SingleCellExperiment class with 895 cells and 2001 genes.
+#'
+#' @name cellbench
+#' @docType data
+#'
+"cellbench"
