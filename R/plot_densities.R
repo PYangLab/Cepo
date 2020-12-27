8b9ff485d34ebe2cb03fa959eb089837ab9283b1
Kevin Wang <kevin.wang@sydney.edu.au>
2020-12-27 18:27:46 +1100

    rename plotDensity

--- R/plot_densities.R
+++ R/plot_densities.R
@@ -7,7 +7,7 @@
 #' indicating the name of the assays(x) element which stores the expression data (i.e., assays(x)$name_assays_expression).
 #' We strongly encourage using normalized data, such as counts per million (CPM) or log-CPM.
 #' @param celltype a character, indicating the name of the cell type to plot. Default is NULL which selects all celltypes in the dx_list.
-#' @param celltype_col a character, indicating the name of the name of the cell type column in the colData(x).
+#' @param celltypeColumn a character, indicating the name of the name of the cell type column in the colData(x).
 #' @param genes a character vector, indicating the name of the genes to plot. Default to NULL, so that 2 top genes from each celltype will be plotted.
 #' @param n_genes number of top genes from each celltype to plot. Default to 2.
 #' @param color a named color vector. The names should correspond to the `celltype` argument above
@@ -18,7 +18,9 @@
 #' @importFrom SingleCellExperiment colData
 #' @importFrom SummarizedExperiment assays
 #' @importFrom grDevices rainbow
+#' @importFrom patchwork plot_layout wrap_plots
 #' @importFrom rlang .data
+#' @importFrom reshape2 melt
 #' @return A \code{\link{ggplot}} object.
 #' @export
 #' @examples
@@ -27,23 +29,22 @@
 #' cellbench
 #' ds_res <- Cepo(logcounts(cellbench), cellbench$celltype)
 #'
-#' plot_densities(
+#' plotDensities(
 #'   x = cellbench,
 #'   dx_list = ds_res,
 #'   assay = 'logcounts',
-#'   celltype_col = 'celltype'
+#'   celltypeColumn = 'celltype'
 #' )
 #'
-#' plot_densities(
+#' plotDensities(
 #'   x = cellbench,
 #'   dx_list = ds_res,
 #'   genes = c('PLTP', 'CPT1C', 'MEG3', 'SYCE1', 'MICOS10P3', 'HOXB7'),
 #'   assay = 'logcounts',
-#'   celltype_col = 'celltype'
+#'   celltypeColumn = 'celltype'
 #' )
-plot_densities <- function(x, dx_list, n_genes = 2, assay = "logcounts", celltype_col, 
+plotDensities <- function(x, dx_list, n_genes = 2, assay = "logcounts", celltypeColumn, 
     celltype = NULL, genes = NULL, plotType = c("histogram", "density"), color = NULL) {
-    # browser()
     if (!is.null(genes)) {
         n_genes <- NULL
     }
@@ -54,7 +55,7 @@ plot_densities <- function(x, dx_list, n_genes = 2, assay = "logcounts", celltyp
     plotType <- match.arg(plotType)
     
     stopifnot((is(x, "SummarizedExperiment") | is(x, "SingleCellExperiment")), is.character(assay), 
-        length(assay) == 1L, is.character(celltype_col), length(celltype_col) == 
+        length(assay) == 1L, is.character(celltypeColumn), length(celltypeColumn) == 
             1L, length(plotType) == 1L)
     
     # exprsMatrix:
@@ -65,11 +66,11 @@ plot_densities <- function(x, dx_list, n_genes = 2, assay = "logcounts", celltyp
     exprsMatrix <- SummarizedExperiment::assays(x)[[assay]]
     
     ## extract cell type labels
-    if (celltype_col %notin% colnames(colData(x))) {
+    if (celltypeColumn %notin% colnames(colData(x))) {
         message("'cell-type label' not found in colnames(colData(x))")
         return(NULL)
     }
-    celltype_label <- colData(x)[[celltype_col]]
+    celltype_label <- colData(x)[[celltypeColumn]]
     if (any(celltype_label %notin% celltype)) {
         # if idx_celltype FALSE:
         message("Some supplied celltypes are not found in the data")
@@ -120,47 +121,46 @@ plot_densities <- function(x, dx_list, n_genes = 2, assay = "logcounts", celltyp
         names(color) <- unique(celltype)
     }
     
+    plotmat <- exprsMatrix[genes, celltype_label %in% celltype, drop = FALSE]
     
-    # Density plot:
-    gg.density.all <- lapply(celltype, function(cty) {
-        Dx <- genes
-        n <- length(Dx)
-        df <- as.data.frame(t(exprsMatrix[Dx, celltype_label %in% cty, drop = FALSE]))
-        df$celltype <- as.character(cty)
-        
-        my_col <- color
-        my_col <- my_col[[cty]]
-        
-        listGGDensity <- lapply(seq_len(n), function(i) {
-            dftoplot <- df[, c(i, ncol(df)), drop = FALSE]
-            genename <- colnames(dftoplot)[[1]]
-            colnames(dftoplot) <- c("gene", "celltype")
-            
-            
-            ggBase <- ggplot2::ggplot(dftoplot, aes(x = .data$gene)) + ggplot2::facet_wrap(~celltype, 
-                scales = "free_y", dir = "v") + ggplot2::ggtitle(genename) + ggplot2::theme_classic() + 
-                ggplot2::theme(axis.title.x = element_blank(), axis.title.y = element_blank(), 
-                  strip.background = element_blank(), strip.text.x = element_blank(), 
-                  legend.position = "bottom")
-            
-            if (plotType == "histogram") {
-                ggDensity <- ggBase + ggplot2::geom_histogram(aes(y = .data$..density..), 
-                  colour = "black", fill = "white", bins = 15) + ggplot2::geom_density(alpha = 0.2, 
-                  fill = my_col)
-            } else {
-                ggDensity <- ggBase + ggplot2::geom_density(alpha = 0.2, fill = my_col)
-            }
-            
-            return(ggDensity)
-        })
-        return(listGGDensity)
-    })
-    n <- unique(vapply(gg.density.all, length))
-    gg.list <- lapply(gg.density.all, function(x) do.call(ggpubr::ggarrange, c(x, 
-        ncol = n)))
-    g <- do.call(ggpubr::ggarrange, c(gg.list, nrow = length(celltype)))
+    annotateDf <- data.frame(
+      cellNames = colnames(plotmat),
+      celltypeAnnotate = celltype_label[celltype_label %in% celltype]
+    )
     
-    g
+    plotdf <- merge(
+      x = reshape2::melt(plotmat, varnames = c("geneNames", "cellNames")), 
+      y = annotateDf, 
+      by = "cellNames")
+    
+    plotdfSplit <- split.data.frame(x = plotdf, f = plotdf$geneNames)
+    
+    ggList <- lapply(
+      X = plotdfSplit, 
+      FUN = function(thisCellTypePlotDf){
+        ggBase <- ggplot2::ggplot(data = thisCellTypePlotDf, ggplot2::aes(x = .data$value)) +
+          ggplot2::facet_wrap(~ celltypeAnnotate, scales = "free", ncol = 1) + 
+          ggplot2::theme_classic() + 
+          ggplot2::labs(title = unique(thisCellTypePlotDf$geneNames),
+                        fill = "Cell type") +
+          ggplot2::theme(axis.title.x = element_blank(), 
+                         axis.title.y = element_blank(), 
+                         strip.background = element_blank(),
+                         strip.text.x = element_blank(),
+                         legend.position = "bottom",
+                         plot.title = element_text(hjust = 0.5))
+        
+        result <- ggBase +
+          ggplot2::geom_histogram(aes(y = .data$..density..), 
+                                  colour = "black", fill = "white", bins = 15) + 
+          ggplot2::geom_density(aes(fill = .data$celltypeAnnotate), alpha = 0.2) +
+          scale_fill_manual(values = color[celltype])
+        return(result)
+      })
+    finalPlot <- patchwork::wrap_plots(ggList, nrow = 1) + 
+      patchwork::plot_layout(guides = "collect") & 
+      ggplot2::theme(legend.position = 'bottom')
+    return(finalPlot)
 }
 
 `%notin%` <- Negate(`%in%`)
---
+++ tests/testthat/test-plotDensity.R
@@ -0,0 +1,19 @@
+library(SingleCellExperiment)
+data('cellbench', package = 'Cepo')
+cellbench
+ds_res <- Cepo(logcounts(cellbench), cellbench$celltype)
+
+expect_message(plotDensities(
+  x = cellbench,
+  dx_list = ds_res,
+  assay = 'logcounts',
+  celltypeColumn = 'celltype'
+))
+
+plotDensities(
+  x = cellbench,
+  dx_list = ds_res,
+  genes = c('PLTP', 'CPT1C', 'MEG3', 'SYCE1', 'MICOS10P3', 'HOXB7'),
+  assay = 'logcounts',
+  celltypeColumn = 'celltype'
+)
