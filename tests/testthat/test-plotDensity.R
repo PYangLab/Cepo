4f3758b71d36437841329106a642c9a93e486675
Kevin Wang <kevin.wang@sydney.edu.au>
2020-12-28 23:48:45 +1100

    fix bug in unit test

--- tests/testthat/test-plotDensity.R
+++ tests/testthat/test-plotDensity.R
@@ -5,14 +5,14 @@ ds_res <- Cepo(logcounts(cellbench), cellbench$celltype)
 
 expect_message(plotDensities(
   x = cellbench,
-  dx_list = ds_res,
+  cepoOutput = ds_res,
   assay = 'logcounts',
   celltypeColumn = 'celltype'
 ))
 
 plotDensities(
   x = cellbench,
-  dx_list = ds_res,
+  cepoOutput = ds_res,
   genes = c('PLTP', 'CPT1C', 'MEG3', 'SYCE1', 'MICOS10P3', 'HOXB7'),
   assay = 'logcounts',
   celltypeColumn = 'celltype'
