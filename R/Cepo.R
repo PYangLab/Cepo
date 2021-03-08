9d7c3c532a746a7463ca29c909bac9fca8f21f3e
Hani Kim <62005528+HaniJieunKim@users.noreply.github.com>
2021-03-08 14:06:51 +1100

    fix bug

--- R/Cepo.R
+++ R/Cepo.R
@@ -154,7 +154,7 @@ rowSds_withnames <- function(mat) {
 }
 
 rowSums_withnames <- function(mat) {
-    result <- DelayedMatrixStats::rowMeans2(mat)
+    result <- DelayedMatrixStats::rowSum2(mat)
     names(result) <- rownames(mat)
     return(result)
 }
