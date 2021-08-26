ec97b7bb842bab2e1365b745e00cfd2fafa172bf
Kevin Wang <kevin.wang@sydney.edu.au>
2021-08-26 17:44:28 +1000

    additionals tests

--- tests/testthat/test-plotDensity.R
+++ tests/testthat/test-plotDensity.R
@@ -17,3 +17,49 @@ plotDensities(
   assay = 'logcounts',
   celltypeColumn = 'celltype'
 )
+
+plotDensities(
+  x = cellbench,
+  cepoOutput = ds_res,
+  genes = c('PLTP', 'CPT1C', 'MEG3', 'SYCE1', 'MICOS10P3', 'HOXB7'),
+  assay = 'logcounts',
+  plotType = "density",
+  celltypeColumn = 'celltype'
+)
+
+expect_null(
+  plotDensities(
+    x = cellbench,
+    cepoOutput = ds_res,
+    assay = 'not_exist',
+    celltypeColumn = 'celltype'
+  ))
+
+expect_null(
+  plotDensities(
+    x = cellbench,
+    cepoOutput = ds_res,
+    assay = 'logcounts',
+    celltypeColumn = 'not_exist'
+  ))
+
+## Too many genes
+expect_null(
+  plotDensities(
+    x = cellbench,
+    cepoOutput = ds_res,
+    genes = rownames(ds_res$stats)[1:11],
+    assay = 'logcounts',
+    celltypeColumn = 'celltype'
+  ))
+
+
+## genes missing in data
+expect_null(
+  plotDensities(
+    x = cellbench,
+    cepoOutput = ds_res,
+    genes = c("not_exist"),
+    assay = 'logcounts',
+    celltypeColumn = 'celltype'
+  ))
